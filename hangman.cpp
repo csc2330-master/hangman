f21e73f6dbfa1e086adefed3a9859751307cf7d7
Carlos R. Arias <arias@spu.edu>
2019-10-21 09:50:48 -0700

    Finished the menu, but I don't like it, going to change to function.

--- hangman.cpp
+++ hangman.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
 
-
+void AddNewWord();
 
 int main(int argc, char* argv[]){
+
+    cout << "Welcome Hangman" << endl << endl;
+    int menuOption;
+    do{
+        cout << "1. Add new word" << endl;
+        cout << "2. Play Hangman" << endl;
+        cout << "3. Quit" << endl;
+        cout << "Your option: ";
+        cin >> menuOption;
+    }while (menuOption != 3);
 
 
 
 	return 0;
+}
+void AddNewWord(){
+
+
 }
